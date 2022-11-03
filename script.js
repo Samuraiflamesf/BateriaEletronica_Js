9fc22625c70e6016e7917b8d10e8d7f32a946b4e
Bernardo Nogueira <Samuraiflamesf@gmail.com>
2022-11-02 23:25:16 -0300

    done

---
+++ script.js
@@ -0,0 +1,40 @@
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLowerCase());
+});
+
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+    console.log(songArray);
+  }
+});
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+  if (audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+  if (keyElement) {
+    keyElement.classList.add("active");
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  songArray.forEach((item) => {
+    setTimeout(() => {
+      playSound(`numpad${item}`);
+    }, wait);
+    wait += 200;
+  });
+}
